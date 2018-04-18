86052c898cb3a68bf911b8728a174d4b32c9d4b3
Shambhavi-Salian <Shambhavi-Salian@github.com>
2018-04-18 18:46:24 +0530

    Removed unwanted files

--- leave_appz/Leave_appz/iOS/CustomButtonIos.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace Leave_appz.iOS
-{
-    public class CustomButtonIos
-    {
-        //Nothing Is required Now 
-        //If you need to add code similar to - "CustomEntryIos.cs"
-    }
-}
